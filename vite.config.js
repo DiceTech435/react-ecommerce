879523663d9268b9a4f4a0c6a19acd50e8ca91a9
DiceTech453 <amevyematthew@gmail.com>
2026-04-26 12:11:17 +0100

    Add server proxy config for /api and /images

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Server proxy configuration
+  server: {
+    proxy: {
+      '/api': { //if url starts with /api, redirect to this target
+        target: 'http://localhost:3000'
+      },
+      './images': { //if url starts with /images, redirect to this target
+        target: 'http://localhost:3000'
+      }
+    }
+  }
 })
