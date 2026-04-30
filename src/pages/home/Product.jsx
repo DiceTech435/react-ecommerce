3b7805351221ecf93bda100f52223fe497906cd1
DiceTech453 <amevyematthew@gmail.com>
2026-04-30 03:00:07 +0100

    Create integration test for produc component

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -12,7 +12,7 @@ export function Product({ product, loadCart }) {
       //   quantity: quantity,
       quantity,
     });
-    
+
     setAddMessage("Added to cart");
 
     setTimeout(() => {
@@ -33,6 +33,7 @@ export function Product({ product, loadCart }) {
         <div className="product-image-container">
           <img
             className="product-image"
+            data-testid="product-image"
             src={product.image}
             alt={product.name}
           />
@@ -43,6 +44,7 @@ export function Product({ product, loadCart }) {
         <div className="product-rating-container">
           <img
             className="product-rating-stars"
+            data-testid="product-rating-stars-image"
             src={`images/ratings/rating-${product.rating.stars * 10}.png`}
           />
           <div className="product-rating-count link-primary">
